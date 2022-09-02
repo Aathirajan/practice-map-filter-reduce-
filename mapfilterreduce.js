a0dd24937fdfa905bad52c025262a317d4601784
Aathi Rajan S <99598685+Aathirajan@users.noreply.github.com>
2022-09-02 09:24:11 +0530

    Add files via upload

---
+++ mapfilterreduce.js
@@ -0,0 +1,47 @@
+// let x=(words)=>
+//   {
+//     let res={};
+//     let word =words.split(' ');
+//     console.log(word)
+//     for (let i of word)
+//       {
+//         if (i in res){
+//           res[i]+=1;
+//         }
+//         else{
+//           res[i]=1;
+
+//         }
+//       }
+//     return res
+//   }
+// console.log(x('hi hello welcome hi welcome but not nice you become what you belive'))
+MAP:
+
+// let x=(array)=>{
+//  return array.map(array=>array*2)
+// }
+// console.log(x([1,2,3,4,5,6,7,8,9]))
+filter:
+
+// let x=(array)=>{
+//   return array.filter(array=>array>3)
+// }
+// console.log(x([1,2,3,4,5,6,7,8,9,10]))
+
+// let x =(array)=>{
+//   return array.filter(array=>array>3)
+// }
+// console.log(x([1,2,3,4,5,6,7,8,9,10,11,12]))
+
+REDUCE
+// let x=(a,b)=>
+//   {return a*b}
+// let z=[1,2,3]
+// let y =z.reduce(x);
+// console.log(y)
+// let x=(a,b)=>a+b
+
+// let y = [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 10]
+// let z = y.reduce(x,-56)
+// console.log(z)
